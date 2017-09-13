036407733b64cfe82f009e24c3641a82f0f20900
goldentom42 <ogrellier@free.fr>
2017-09-13 15:25:09 +0200

    Add typing 3.6 requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='py_ml_utils',
       author='Olivier Grellier',
       author_email='goldentom42@gmail.com',
       license='Apache 2.0',
-      install_requires=['numpy>=1.13.1', 'scipy>=0.19.1', 'scikit_learn>=0.19', 'pandas>=0.20.2'],
+      install_requires=['numpy>=1.13.1', 'scipy>=0.19.1', 'scikit_learn>=0.19', 'pandas>=0.20.2', 'typing>=3.6'],
       packages=PACKAGES,
       zip_safe=False)
